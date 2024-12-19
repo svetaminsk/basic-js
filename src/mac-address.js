dc6f5cc82a7b1b88e4bfc2cd8a8f1681ed4e9090
Svetlana Evdokimova <7757331@gmail.com>
2024-12-19 20:00:03 +0300

    feat: resolve get-email-domain, hanoi-tower and mac-address tasks

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  const correct = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '-'];
+  const result = n.split('');
+  for (let i = 0; i < result.length; i += 1) {
+    if (!correct.includes(result[i])) {
+      return false;
+    }
+  }
+  return true;
 }
 module.exports = {
   isMAC48Address
