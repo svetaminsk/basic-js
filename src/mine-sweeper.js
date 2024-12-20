a5af18acbba770604c7d0619888fe972d72bdf56
Svetlana Evdokimova <7757331@gmail.com>
2024-12-20 18:00:54 +0300

    feat: resolve recursive depth, mine-sweeper, matrix-elements-sum tasks

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const result = matrix.map((item) => [...item]);
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      if (result[i][j]) {
+        result[i][j] = 1;
+      } else {
+        result[i][j] = 0;
+      }
+    }
+  }
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      if (matrix[i][j]) {
+        if (i !== matrix.length - 1 && !matrix[i + 1][j]) {
+          result[i + 1][j] += 1;
+        }
+        if (i !== 0 && !matrix[i - 1][j]) {
+          result[i - 1][j] += 1;
+        }
+        if (j !== matrix[j].length - 1 && !matrix[i][j + 1]) {
+          result[i][j + 1] += 1;
+        }
+        if (j !== 0 && !matrix[i][j - 1]) {
+          result[i][j - 1] += 1;
+        }
+        if (i !== matrix.length - 1 && j !== 0 && !matrix[i + 1][j - 1]) {
+          result[i + 1][j - 1] += 1;
+        }
+        if (i !== 0 && j !== matrix[j].length - 1 && !matrix[i - 1][j + 1]) {
+          result[i - 1][j + 1] += 1;
+        }
+        if (i !== matrix.length - 1 && j !== matrix[j].length - 1 && !matrix[i + 1][j + 1]) {
+          result[i + 1][j + 1] += 1;
+        }
+        if (i !== 0 && j !== 0 && !matrix[i - 1][j - 1]) {
+          result[i - 1][j - 1] += 1;
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) return 0;
+    let result = 0;
+    arr.forEach((item) => {
+      if (Array.isArray(item)) {
+        result = Math.max(this.calculateDepth(item), result);
+      }
+    })
+    return result + 1;
   }
 }
 
